200a5bea54f8a7eba1085d1a885d8faf6dee89ad
Andy Weiss <andyweiss1982@gmail.com>
2019-07-17 14:49:59 -0400

    fix search

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -17,9 +17,8 @@ class SearchBar extends React.Component {
   }
 
   render(){
-    console.log(this.props)
     return(
-      <form id="resize" onSubmit={this.handleSubmit} autoComplete="off" className={this.state.input ? "filled" : ""}> 
+      <form id="resize" onSubmit={this.handleSubmit} autoComplete="off" className={this.state.input ? "filled" : ""}>
         <input id="navsrch" type="text" onChange={this.handleChange} />
         <i className="fa fa-search"></i>
       </form>
